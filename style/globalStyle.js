c43e22760420d26f5a9e690de20b846279af60c5
subin313 <82709790+subin313@users.noreply.github.com>
2025-06-03 18:18:35 +0900

    글 스타일 변경

--- style/globalStyle.js
+++ style/globalStyle.js
@@ -5,7 +5,7 @@ const menuListStyle = `md:ml-10 text-base leading-snug text-surface hover:text-g
 const mobileMenuStyle = `m-0 block py-4 px-6`;
 
 // blog style과 notebook style
-const posth1Style = `text-[40px] font-bold mb-4 mt-6 border-b border-graylv2 pb-2.5`;
+const posth1Style = `text-[100px] font-bold mb-4 mt-6 border-b border-graylv2 pb-2.5`;
 const posth2Style = `text-[32px] font-bold mb-4 mt-6 border-b border-graylv2 pb-2.5`;
 const posth3Style = `text-[28px] font-bold mb-4 mt-6 border-b border-graylv2 pb-2.5`;
 const posth4Style = `text-2xl font-bold mb-2 mt-4`;
